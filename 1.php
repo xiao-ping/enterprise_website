e1b16bc2e11e53c414e0106634ca6568faeb65a2
xiao-ping <66455384+xiao-ping@users.noreply.github.com>
2020-06-10 16:24:08 +0800

    这是上传的文件
    上传1.php文件

---
+++ 1.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>无标题文档</title>
+</head>
+
+<body>
+<?php
+$a=15;
+if($a%17==0){
+	echo "1";
+}else{
+	echo "0";
+}
+?>
+</body>
+</html>
